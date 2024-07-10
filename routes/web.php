389c637fbddbec709ff8c30e79564d3b7037877a
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-10 18:09:39 +0600

    Fallback Routes

--- routes/web.php
+++ routes/web.php
@@ -8,32 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// Route Methods
-/**
- * 1. Get - if we wanat to get data
- * 2. POST - submit/store data
- * 3. PUT - if update
- * 4. PATCH - if udate
- * 5. DELETE - when we delete something
- */
-
-Route::get('get-route', function() {
-    return;
-});
-
-Route::post('post-route', function() {
-    return;
-});
-
-Route::put('put-route', function() {
-    return;
-});
-
-Route::patch('patch-route', function() {
-    return;
-});
-
-Route::delete('delete-route', function() {
-    return;
+// Fallback route
+Route::fallback(function() {
+    return "Ooops we couldnt find the page!";
 });
