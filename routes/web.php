7e074cad4dca19a4d7af82fd6dd05b28665d1c15
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-10 15:38:39 +0600

    Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return "This is a About page";
+});
+
+Route::get('/user/{id}/{slug}', function($id, $slug) {
+    return "Hello user " . $id. '-'.$slug;
+});
