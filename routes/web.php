e7dc0bf26d2c9cb597d262e19120a1b82cb61ab5
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-10 15:21:15 +0600

    chore: Basic routeing

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return "This is a About page";
+});
+
+
