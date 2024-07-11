d34b97fcb235b671d3dffeb925d0cf0cc2e37977
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-11 14:54:41 +0600

    Conditional Rendering in Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,8 +8,10 @@
         <h1 class="fw-light">Album example</h1>
         <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
         <p>
+          @if (true)
           <a href="#" class="btn btn-primary my-2">Main call to action</a>
           <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          @endif
         </p>
       </div>
     </div>
