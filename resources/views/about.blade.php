4ddbe8590d6f7b6452baeca9a9fc584bc5c21e24
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-11 14:37:48 +0600

    Including Sub-views

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,8 @@
 @extends('app')
 
 @section('contents')
+@include('gallery')
+
 <section class="py-5 text-center container">
     <div class="row py-lg-5">
       <div class="col-lg-6 col-md-8 mx-auto">
