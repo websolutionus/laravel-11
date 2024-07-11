73979ede1c8536f6dcda5d144bcebd0b4d4a3e05
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-11 11:23:56 +0600

    Passing Data to Blade

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -7,27 +7,18 @@
     <title>Document</title>
 </head>
 
-@php
-  $title = "Contact Page!";
-@endphp
-
 <body>
-    {{-- <h2>{{ $title }}</h2> --}}
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod cupiditate cumque doloribus temporibus, quasi quae reiciendis qui, inventore consectetur a vitae debitis? Magni facilis quisquam dolore sapiente sed nisi accusamus!
+    <h2>{{ $title }}</h2>
+    {{ $description }}
+    <ul>
+      @foreach ($books as $book)    
+      <li>{{ $book }}</li>
+      @endforeach
+      <br>
+      <br>
+      @for ($i = 0; $i < count($books); $i++)
+      <li>{{ $books[$i] }}</li>
+      @endfor
+    </ul>
 </body>
 </html>
-
-
-{{-- @if()
-
-@elseif()
-
-@endif
-
-@for()
-
-@endfor
-
-@foreach()
-
-@endforeach --}}
--- routes/web.php
+++ routes/web.php
@@ -10,5 +10,10 @@ Route::get('/', function () {
 
 
 Route::get('/contact', function(){
-    return view('contact.index');
+    $title = "Contact Page!";
+    $description = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod cupiditate cumque doloribus temporibus, quasi quae reiciendis qui, inventore consectetur a vitae debitis? Magni facilis quisquam dolore sapiente sed nisi accusamus!";
+
+    $books = ["Deep work", "Steal like a artist", "Story Brand"];
+
+    return view('contact.index', ['title' => $title, 'description' => $description, 'books' => $books]);
 });
