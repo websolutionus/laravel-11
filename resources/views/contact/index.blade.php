96ff1502229923d1f6c719431f6478f1b96d7576
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-11 11:01:05 +0600

    Blade Directives

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -6,8 +6,28 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
+@php
+  $title = "Contact Page!";
+@endphp
+
 <body>
-    <h2>Contact Page!</h2>
+    {{-- <h2>{{ $title }}</h2> --}}
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod cupiditate cumque doloribus temporibus, quasi quae reiciendis qui, inventore consectetur a vitae debitis? Magni facilis quisquam dolore sapiente sed nisi accusamus!
 </body>
 </html>
+
+
+{{-- @if()
+
+@elseif()
+
+@endif
+
+@for()
+
+@endfor
+
+@foreach()
+
+@endforeach --}}
