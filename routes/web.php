3ba04802f97e43e4bae9002e7c02db3d24be0388
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-11 13:00:39 +0600

    Extending Blade

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/contact', function(){
-    $title = "Contact Page!";
-    $description = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod cupiditate cumque doloribus temporibus, quasi quae reiciendis qui, inventore consectetur a vitae debitis? Magni facilis quisquam dolore sapiente sed nisi accusamus!";
-
-    $books = ["Deep work", "Steal like a artist", "Story Brand"];
-
-    return view('contact.index', ['title' => $title, 'description' => $description, 'books' => $books]);
+Route::get('/about', function () {
+    return view('about');
 });
