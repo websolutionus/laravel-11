bada654413e9a5bede286d5ea6a02a77a1c9f2f9
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-14 15:33:13 +0600

    Important Migration Commands

--- database/migrations/2024_07_14_064024_create_blogs_table.php
+++ database/migrations/2024_07_14_064024_create_blogs_table.php
@@ -15,15 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description');
-            $table->timestamps();
-            // integer
-            // char
-            // bigInteger
-            // float
-            // date
-            // boolean
-            // json
-
+            $table->boolean('status');
         });
     }
 
