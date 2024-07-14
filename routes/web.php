165ff90358aac261cbf86c60b62b417deadbf94f
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-14 09:39:44 +0600

    Single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SingleActionController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -8,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [HomeController::class, 'showAboutPage']);
+
+Route::get('/single-action', SingleActionController::class);
