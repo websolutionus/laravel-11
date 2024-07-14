4a43591ae976c20cc6d264a50f93f17e00ce8817
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-14 11:04:57 +0600

    Resource Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SingleActionController;
 use Illuminate\Support\Facades\Route;
@@ -11,3 +12,17 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [HomeController::class, 'showAboutPage']);
 
 Route::get('/single-action', SingleActionController::class);
+
+// BLOG
+// Create
+// Read
+// Update
+// Delete
+// (CRUD)
+
+// Route::post('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+// Route::get('/blog/show', [BlogController::class, 'show'])->name('blog.show');
+// Route::post('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+// Route::post('/blog/create', [BlogController::class, 'create'])->name('blog.create');
+
+Route::resource('/blog', BlogController::class);
