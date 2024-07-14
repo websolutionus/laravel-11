b74f2cbdc40e3a74d52fe4d6ef6dd4bd5ce60e18
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-14 13:38:53 +0600

    Table Data Types

---
+++ database/migrations/2024_07_14_064024_create_blogs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->timestamps();
+            // integer
+            // char
+            // bigInteger
+            // float
+            // date
+            // boolean
+            // json
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
