bb3ee5c88f8bc8a7a2074b419d27d0d9aa08552f
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-15 16:39:13 +0600

    Retrieving a List of Column Values

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,35 +10,8 @@ class HomeController extends Controller
     function index()
     {
 
-        // Insert new data
-        // DB::table('users')->insert(
-        //     [
-        //         [
-        //             'name' => 'Artik123',
-        //             'email' => 'artik123@gmail.com',
-        //             'password' => '1234'
-        //         ], 
-        //         [
-        //             'name' => 'Jhohan',
-        //             'email' => 'johan@gmail.com',
-        //             'password' => '1234'
-        //         ]
-        //     ]
-        // );
-
-        // Get data from db
-        // $users = DB::table('users')->where('id', '>', 2)->get();
-        // return $users;
-
-        // Update row from db
-        // DB::table('users')->where('id', 6)->update([
-        //     'email' => 'test@gmail.com',
-        //     'name' => 'test user'
-        // ]);
-
-        // delete row from db
-        DB::table('users')->where('id', '>', 1)->delete();
-        
+        $blogs = DB::table('blogs')->pluck('title', 'id')->toArray();
+        dd($blogs);
 
         return view('welcome');
     }
