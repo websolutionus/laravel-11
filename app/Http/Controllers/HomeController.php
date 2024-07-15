106603a24c28795f938850ffdba9f297313c8dd1
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-15 11:57:42 +0600

    Creating New Data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    function index() {
+    function index()
+    {
+
+        DB::table('users')->insert(
+            [
+                [
+                    'name' => 'Artik123',
+                    'email' => 'artik123@gmail.com',
+                    'password' => '1234'
+                ], 
+                [
+                    'name' => 'Jhohan',
+                    'email' => 'johan@gmail.com',
+                    'password' => '1234'
+                ]
+            ]
+        );
+
         return view('welcome');
     }
 
-    function showAboutPage() {
+    function showAboutPage()
+    {
         return view('about');
     }
 }
