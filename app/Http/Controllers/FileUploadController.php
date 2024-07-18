c8599e4754f87c7b09c730ae9a9a814e86dca793
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-18 15:13:32 +0600

    Change File Name Before Save

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\File;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Str;
 
 class FileUploadController extends Controller
 {
@@ -17,10 +18,16 @@ class FileUploadController extends Controller
 
         // $file = Storage::disk('local')->put('/', $request->file('file'));
         // $file = $request->file('file')->store('/', 'local');
-        $file = $request->file('file')->store('/', 'dir_public');
+
+        $file = $request->file('file');
+        $customName = 'laravel_'. Str::uuid();
+        $ext = $file->getClientOriginalExtension();
+        $fileName = $customName. '.' . $ext;
+
+        $path = $file->storeAs('/', $fileName, 'dir_public');
 
         $fileStore = new File();
-        $fileStore->file_path = '/uploads/'.$file;
+        $fileStore->file_path = '/uploads/'.$path;
         $fileStore->save();
 
         dd('stored');
