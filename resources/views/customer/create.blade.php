7aab999cd4bd5b79eca9b0721d8d6a0f8180d43f
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-25 15:04:39 +0600

    CRUD - Submit Form and Validation

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -1,65 +1,80 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row justify-content-center mt-5">
-    <div class="col-md-8">
-        <h3>Customers</h3>
-        <div class="card">
-            <div class="card-header">
-                <div class="row">
-                    <div class="col-md-2">
-                        <a href="{{ route('home') }}" class="btn" style="background-color: #4643d3; color: white;"><i class="fas fa-chevron-left"></i> Back</a>
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-8">
+            <h3>Customers</h3>
+            @if($errors->any()) 
+                @foreach ($errors->all() as $error)
+                    <div class="alert alert-danger">{{ $error }}</div>  
+                @endforeach
+            @endif
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <a href="{{ route('home') }}" class="btn" style="background-color: #4643d3; color: white;"><i
+                                    class="fas fa-chevron-left"></i> Back</a>
+                        </div>
+
                     </div>
 
                 </div>
+                <div class="card-body">
+                    <form action="{{ route('customers.store') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-md-12 mb-3">
+                                <div class="form-group">
+                                    <label for="">Image</label>
+                                    <input type="file" class="form-control" name="image">
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="">First Name</label>
+                                    <input type="text" class="form-control" name="first_name" value="{{ old('first_name') }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="">Last Name</label>
+                                    <input type="text" class="form-control" name="last_name" value="{{ old('last_name') }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="">Email</label>
+                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="">Phone</label>
+                                    <input type="text" class="form-control" name="phone" value="{{ old('phone') }}">
+                                </div>
+                            </div>
 
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-md-12 mb-3">
-                        <div class="form-group">
-                            <label for="">Image</label>
-                            <input type="file" class="form-control">
+                            <div class="col-md-12 mb-3">
+                                <div class="form-group">
+                                    <label for="">Bank Account Number</label>
+                                    <input type="text" class="form-control" name="bank_account_number" value="{{ old('bank_account_number') }}">
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <div class="form-group">
+                                    <label for="">About</label>
+                                    <textarea name="about" class="form-control">{{ old('about') }}</textarea>
+                                </div>
+                            </div>
+                            <div class="col-md-12 mb-3">
+                                <button type="submit" class="btn btn-dark"><i class="fas fa-save"></i> Create</button>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <div class="form-group">
-                            <label for="">First Name</label>
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <div class="form-group">
-                            <label for="">Last Name</label>
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <div class="form-group">
-                            <label for="">Email</label>
-                            <input type="email" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-3">
-                        <div class="form-group">
-                            <label for="">Phone</label>
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
-
-                    <div class="col-md-12 mb-3">
-                        <div class="form-group">
-                            <label for="">Bank Account Number</label>
-                            <input type="text" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-md-12 mb-3">
-                        <button type="submit" class="btn btn-dark"><i class="fas fa-save"></i> Create</button>
-                    </div>
+                    </form>
 
                 </div>
             </div>
         </div>
     </div>
-</div> 
 @endsection
