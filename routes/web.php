fb2ea0db01576b77c82203379453387263d38b92
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-26 15:51:55 +0600

    CRUD - Soft Delete Feature (Part - 1)

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
+Route::get('customers/trash', [CustomerController::class, 'trashIndex'])->name('customers.trash');
 Route::resource('customers', CustomerController::class);
