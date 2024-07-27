bcf4de9124bdbc19fb343ce22be073b975147314
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-27 13:49:27 +0600

    Joins - Inner Join

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/join', function() {
+    $usersWithOrders = \DB::table('users')
+        ->join('orders', 'users.id', '=', 'orders.user_id')
+        ->select('users.*', 'orders.*')
+        ->get();
+
+    dd($usersWithOrders);
+});
