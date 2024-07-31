8fa7af742b222b9a7941b072f3262d551650df52
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-07-31 11:32:37 +0600

    Responses - Redirect Responses

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    function index() {
+        // return to_route('post.create');
+    }
+
+    function create() {
+        // return redirect()->back();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/post', [PostController::class, 'index'])->name('post.index');
+
+Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
+
