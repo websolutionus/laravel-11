5deb53bf6de7a0f912612860f26a75a3d663f081
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-08-01 15:58:56 +0600

    Blade Components - Creating Blade Components

--- routes/web.php
+++ routes/web.php
@@ -9,23 +9,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-
-Route::get('send-mail', function() {
-    return view('send-mail');
-});
-
-Route::post('send-mail', function(Request $request) {
-    // Mail::raw($request->message, function($message) use ($request) {
-    //     $message->to($request->email)
-    //     ->subject('Laravel Test Email');
-    // });
-
-    // with queue
-    Mail::to($request->email)->queue(new SendMail($request->message));
-
-    // without queue
-    Mail::to($request->email)->send(new SendMail($request->message));
-
-    dd("Email Sent");
-})->name('send.mail');
