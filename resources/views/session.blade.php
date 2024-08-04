8b96da6869e2ed63ba4cb50d83e273d81d603814
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-08-04 13:20:24 +0600

    Session - Storing data at session

---
+++ resources/views/session.blade.php
@@ -0,0 +1,1 @@
+<h1>Session Page</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('session', function(Request $request) {
+    // request()->session()->put('foo', ['php', 'laravel', 'vue']);
+    // session(['cart' => ['product 1', 'product 2', 'product 3']]);
+    Session::put('test', 'bar');
+
+
+    return view('session');
 });
