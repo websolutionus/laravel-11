bcb8e5580ede3159b6b8fdc9fdf7502db1559128
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-08-04 13:49:21 +0600

    Session - Retrieving data from session

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,12 @@ Route::get('/', function () {
 Route::get('session', function(Request $request) {
     // request()->session()->put('foo', ['php', 'laravel', 'vue']);
     // session(['cart' => ['product 1', 'product 2', 'product 3']]);
-    Session::put('test', 'bar');
+    // Session::put('test', 'bar');
+
+    // $value = $request->session()->get('test');
+    // $value = session('test', 'this is default value');
+    $value = Session::get('test');
+    dd($value);
 
 
     return view('session');
