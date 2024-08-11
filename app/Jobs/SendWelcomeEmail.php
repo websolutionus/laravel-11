2d38f9c833821fd4deb75584bcdbc490584398b8
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-08-11 12:40:08 +0600

    Job -Queueing an email

--- app/Jobs/SendWelcomeEmail.php
+++ app/Jobs/SendWelcomeEmail.php
@@ -2,22 +2,26 @@
 
 namespace App\Jobs;
 
+use App\Mail\WelcomeEmail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
 
 class SendWelcomeEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $user;
+
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct($user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
@@ -25,6 +29,6 @@ class SendWelcomeEmail implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        Mail::to($this->user->email)->send(new WelcomeEmail($this->user));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Jobs\SendWelcomeEmail;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send', function() {
+    $user = User::find(1);
+    dispatch(new SendWelcomeEmail($user));
+    dd('Email sent');
 });
