329c696a2deaefaf2be87e67b0e3d94ba1d24256
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-08-12 15:59:31 +0600

    Broadcasting - Firing Event On Public Channel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Events\NewMessage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,4 +9,10 @@ Route::get('/', function () {
 
 Route::get('messages', function () {
     return view('messages');
+});
+
+Route::get('send-message', function () {
+    event(new NewMessage("this is a new message!"));
+
+    dd('Message Sent!');
 });
