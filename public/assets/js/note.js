e4f44f6065d0907293933f5f9a37331e1da9987e
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-08-22 11:51:00 +0600

    8_Project - Working with Create Feature (Part - 2)

--- public/assets/js/note.js
+++ public/assets/js/note.js
@@ -2,17 +2,31 @@
 const csrf_token = $('meta[name="csrf_token"]').attr('content');
 const base_url = $('meta[name="base_url"]').attr('content');
 const noteTitle = $('.note_title');
+const noteContent = $('.note_content');
+
 
 /** Reusable functions */
-function handleNoteTitle() {
+
+function debounce(func, delay) { // 300ms
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func(...args), delay);
+    }
+}
+
+
+function handleNote() {
     let noteTitleVal = noteTitle.val();
+    let noteContentVal = noteContent.val();
     console.log(noteTitleVal);
     $.ajax({
         method: 'POST',
         url: `${base_url}/notes`,
         data: {
             _token: csrf_token,
-            note_title: noteTitleVal
+            note_title: noteTitleVal,
+            note_content: noteContentVal
         },
         success: function() {},
         error: function(xhr, status, error) {},
@@ -21,5 +35,6 @@ function handleNoteTitle() {
 
 
 $(document).ready(function() {
-    noteTitle.on('input', handleNoteTitle);
+    noteTitle.on('input', debounce(handleNote, 400));
+    noteContent.on('input', debounce(handleNote, 400));
 });
